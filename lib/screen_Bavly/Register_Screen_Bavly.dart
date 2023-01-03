bd54c127b475e5c08a22bd7b7d657fe8240a4822
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 08:32:56 +0200

    design register screen

--- lib/screen_Bavly/Register_Screen_Bavly.dart
+++ lib/screen_Bavly/Register_Screen_Bavly.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_auth_flutter_project/Widget_bavly/Custom_Button_Bavly.dart';
 
 class RegisterScreen_Bavly extends StatefulWidget {
   const RegisterScreen_Bavly({super.key});
@@ -9,10 +10,13 @@ class RegisterScreen_Bavly extends StatefulWidget {
 }
 
 class _RegisterScreen_BavlyState extends State<RegisterScreen_Bavly> {
-  
+  final TextEditingController phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    
+    phoneController.selection = TextSelection.fromPosition(
+      TextPosition(offset: phoneController.text.length),
+    );
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -31,7 +35,7 @@ class _RegisterScreen_BavlyState extends State<RegisterScreen_Bavly> {
                 const SizedBox(
                   height: 20,
                 ),
-                const Text("ٌRegister",
+                const Text("Register",
                     style:
                         TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
                 const SizedBox(
@@ -48,7 +52,64 @@ class _RegisterScreen_BavlyState extends State<RegisterScreen_Bavly> {
                 const SizedBox(
                   height: 20,
                 ),
-                
+                TextFormField(
+                  cursorColor: Colors.purple,
+                  controller: phoneController,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      phoneController.text = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter Phone number",
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                      color: Colors.grey.shade600,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+                    prefixIcon: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(""),
+                      ),
+                    ),
+                    suffixIcon: phoneController.text.length > 9
+                        ? Container(
+                            height: 30,
+                            width: 30,
+                            margin: const EdgeInsets.all(10.0),
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.green),
+                            child: const Icon(
+                              Icons.done,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          )
+                        : null,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton_Bavly(text: "Login", onPressed: () {}),
+                ),
               ],
             ),
           ),
@@ -56,3 +117,4 @@ class _RegisterScreen_BavlyState extends State<RegisterScreen_Bavly> {
       ),
     );
   }
+}
