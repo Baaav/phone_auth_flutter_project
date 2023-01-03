39c1c44f9dae4c5122337dc05ed364f6491874b4
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 20:21:47 +0200

    coding class Auth_Provider_Bavly

--- lib/Provider_Bavly/auth_provider.dart
+++ lib/Provider_Bavly/auth_provider.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Auth_Provider_Bavly extends ChangeNotifier {}
+class Auth_Provider_Bavly extends ChangeNotifier {
+  bool _isSignedIn = false;
+  bool get isSignedIn => isSignedIn;
+
+  Auth_Provider_Bavly() {
+    checkSignIn();
+  }
+
+  void checkSignIn() async {
+    final SharedPreferences s = await SharedPreferences.getInstance();
+    _isSignedIn = s.getBool("is_signesin") ?? false;
+    notifyListeners();
+  }
+}
