fc049c56f17e6fcb0de33c5ca8881df4dbbb843a
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 08:14:39 +0200

    add picture and texts to register screen

---
+++ lib/screen_Bavly/Register_Screen_Bavly.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen_Bavly extends StatefulWidget {
+  const RegisterScreen_Bavly({super.key});
+
+  @override
+  State<RegisterScreen_Bavly> createState() => _RegisterScreen_BavlyState();
+}
+
+class _RegisterScreen_BavlyState extends State<RegisterScreen_Bavly> {
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.purple.shade50),
+                  child: Image.asset("assets/image/image2.png"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text("ٌRegister",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Add your phone number. We'll send you a verification code",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
