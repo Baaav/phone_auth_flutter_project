ee3c3bfe01470c1b6dc396a4100f4555fd847d78
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 07:40:57 +0200

    design button to call in reasable use

--- lib/Widget_bavly/Custom_Button_Bavly.dart
+++ lib/Widget_bavly/Custom_Button_Bavly.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class CustomButton_Bavly extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton_Bavly(
+      {super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.purple),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+            )),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 16),
+        ));
+  }
+}
