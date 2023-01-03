69e07704edc5b120337212c30eb619a34b560485
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 21:29:09 +0200

    coding authprovider

--- lib/Provider_Bavly/auth_provider.dart
+++ lib/Provider_Bavly/auth_provider.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,5 +17,11 @@ class Auth_Provider_Bavly extends ChangeNotifier {
     final SharedPreferences s = await SharedPreferences.getInstance();
     _isSignedIn = s.getBool("is_signesin") ?? false;
     notifyListeners();
+  }
+
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
+    try{} on FirebaseAuthException catch{e}{
+      
+    }
   }
 }
