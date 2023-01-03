8134cb5f7d63316a7e2518b7383e3073248522a7
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 21:35:34 +0200

    firebase auth and fin checksign in is ready

--- lib/Provider_Bavly/auth_provider.dart
+++ lib/Provider_Bavly/auth_provider.dart
@@ -3,11 +3,16 @@ import 'dart:math';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:phone_auth_flutter_project/screen_Bavly/otp_screen-bavly.dart';
+import 'package:phone_auth_flutter_project/util_bavly/util_Bavly.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth_Provider_Bavly extends ChangeNotifier {
   bool _isSignedIn = false;
   bool get isSignedIn => isSignedIn;
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Auth_Provider_Bavly() {
     checkSignIn();
@@ -20,8 +25,28 @@ class Auth_Provider_Bavly extends ChangeNotifier {
   }
 
   void signInWithPhone(BuildContext context, String phoneNumber) async {
-    try{} on FirebaseAuthException catch{e}{
-      
+    try {
+      await _firebaseAuth.verifyPhoneNumber(
+          phoneNumber: phoneNumber,
+          verificationCompleted:
+              (PhoneAuthCredential phoneAuthCredential) async {
+            await _firebaseAuth.signInWithCredential(phoneAuthCredential);
+          },
+          verificationFailed: (error) {
+            throw Exception(error.message);
+          },
+          codeSent: (verificationId, forceResendingToken) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    Otp_Screen_bavly(verificationId: verificationId),
+              ),
+            );
+          },
+          codeAutoRetrievalTimeout: (verificationId) {});
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message.toString());
     }
   }
 }
