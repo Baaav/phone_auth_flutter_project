6b0797cfac33765e55bf9d431748a93d457a9273
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 22:55:19 +0200

    coding otp to can verify

--- lib/screen_Bavly/otp_screen-bavly.dart
+++ lib/screen_Bavly/otp_screen-bavly.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:phone_auth_flutter_project/Widget_bavly/Custom_Button_Bavly.dart';
+import 'package:phone_auth_flutter_project/util_bavly/util_Bavly.dart';
+import 'package:pinput/pinput.dart';
 
 class Otp_Screen_bavly extends StatefulWidget {
   final String verificationId;
@@ -11,6 +14,114 @@ class Otp_Screen_bavly extends StatefulWidget {
 class _Otp_Screen_bavlyState extends State<Otp_Screen_bavly> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child:  const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.purple,
+                ),
+              )
+            : Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: GestureDetector(
+                          onTap: () => Navigator.of(context).pop(),
+                          child: const Icon(Icons.arrow_back),
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        height: 200,
+                        padding: const EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.purple.shade50,
+                        ),
+                        child: Image.asset(
+                          "assets/image2.png",
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Verification",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Enter the OTP send to your phone number",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black38,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 20),
+                      Pinput(
+                        length: 6,
+                        showCursor: true,
+                        defaultPinTheme: PinTheme(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: Colors.purple.shade200,
+                            ),
+                          ),
+                          textStyle: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        onCompleted: (value) {
+                          setState(() {
+                            otpCode = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 25),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: CustomButton_Bavly(
+                          text: "Verify",
+                          onPressed: () {
+                            
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Didn't receive any code?",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black38,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      const Text(
+                        "Resend New Code",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
   }
 }
