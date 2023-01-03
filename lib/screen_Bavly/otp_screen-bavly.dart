79f68e3867eb2bfb23536ae828bce9b97f86ea58
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 21:42:19 +0200

    continue coding otp screeen

---
+++ lib/screen_Bavly/otp_screen-bavly.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Otp_Screen_bavly extends StatefulWidget {
+  final String verificationId;
+  const Otp_Screen_bavly({super.key, required this.verificationId});
+
+  @override
+  State<Otp_Screen_bavly> createState() => _Otp_Screen_bavlyState();
+}
+
+class _Otp_Screen_bavlyState extends State<Otp_Screen_bavly> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
