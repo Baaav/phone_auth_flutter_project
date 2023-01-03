002fbd41faf577fee7616b3ecc19f6d8fb8dec9b
bavlybadry123 <bavlybadry123@gmail.com>
2023-01-03 07:31:25 +0200

    design welcome screen

--- lib/screen_Bavly/Welcome_Screen_Bavly.dart
+++ lib/screen_Bavly/Welcome_Screen_Bavly.dart
@@ -10,6 +10,37 @@ class welcome extends StatefulWidget {
 class _welcomeState extends State<welcome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/image/image1",
+                height: 300,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("Let's get started ",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("Never a better time than now to start.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black,
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
